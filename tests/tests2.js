d86208ee38ed749e6aa4e7047b810e98ac326376
Chris Daley <2166307+chrisdaley@users.noreply.github.com>
2020-12-11 10:41:02 -0500

    Create tests2.js

---
+++ tests/tests2.js
@@ -0,0 +1,15 @@
+test('terminates on cookie', () => {
+    var result = onClientRequest({headers: {
+        "Cookie": "cart=true",
+    }});
+
+    // should not terminate and redirect
+    assertEq(result.terminates, false);
+})
+
+test('no_op on missing cookie', () => {
+    var result = onClientRequest();
+
+    // should terminate and redirect
+    assertEq(result.terminates, true);
+})
