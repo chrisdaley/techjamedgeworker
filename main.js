2905890073ce9aed43295ad2eee0611e8364c637
Chris Daley <2166307+chrisdaley@users.noreply.github.com>
2020-12-11 10:40:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // API endPoint not cookie return empty json response.
-
+import {logger} from 'log';
 import {Cookies, SetCookie} from 'cookies';
 
 export function onClientRequest(request) {
-  console.log("I am totally going to set a header in onClientRequest");
+  
   let cookies = new Cookies(request.getHeader('Cookie'));
   var cartCookie = cookies.get('cart');
   if (!cartCookie) {
@@ -12,15 +12,14 @@ export function onClientRequest(request) {
 }
 
 export function onClientResponse(request, response) {
-    console.log("I am totally going to set a header in onClientResponse");
     response.setHeader('X-Powered-By','Akamai EdgeWorkers');
     response.setHeader('test','2.5.1');
 }
 
 export function onOriginRequest(request) {
-  console.log("I am doing nothing in onOriginRequest");
+  
 }
 
 export function onOriginResponse(request, response) {
-  console.log("I am pretty useless here.");
+  
 }
